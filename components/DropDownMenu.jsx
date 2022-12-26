adcd38f25d798d7f7e91a4e939054a5d4475bed1
KareemE125 <kareemezzat.125@gmail.com>
2022-12-26 02:50:53 +0200

    hover styling

--- components/DropDownMenu.jsx
+++ components/DropDownMenu.jsx
@@ -8,26 +8,26 @@ import { ThemeContext } from '../context/ThemeContext';
 export default function DropDownMenu() {
     const { theme, toggleTheme } = useContext(ThemeContext);
     const router = useRouter();
-    const activeClass = useMemo(() => 'px-4 py-2 hover:bg-[#f3f4f6] font-bold flex justify-center text-xl dark:text-violet-500 transition-all duration-300', []);
-    const inActiveClass = useMemo(() => 'px-4 py-2 flex justify-center hover:bg-[#f3f4f6]', []);
+    const activeClass = useMemo(() => 'px-4 py-2 hover:bg-[#f3f4f6] dark:hover:bg-[#5b21b6] font-bold flex justify-center text-xl dark:text-violet-500 transition-all duration-300', []);
+    const inActiveClass = useMemo(() => 'px-4 py-2 flex justify-center hover:bg-[#f3f4f6] dark:hover:bg-[#5b21b6]', []);
 
 
 
     return <Dropdown style={{ background: theme === 'dark' ? '#5b21b6' : 'black' }} label="Menu">
         <Link className={router.asPath === '/' ? activeClass : inActiveClass} href='/'>
-            <Dropdown.Item className='px-0 pt-0 pb-0'>Home</Dropdown.Item>
+            <Dropdown.Item className='px-0 pt-0 pb-0 dark:hover:bg-[#5b21b6]'>Home</Dropdown.Item>
         </Link>
         <Link className={router.asPath === '/events' ? activeClass : inActiveClass} href='/events'>
-            <Dropdown.Item className='px-0 pt-0 pb-0'>Events</Dropdown.Item>
+            <Dropdown.Item className='px-0 pt-0 pb-0 dark:hover:bg-[#5b21b6]'>Events</Dropdown.Item>
         </Link >
         <Link className={router.asPath === '/tops' ? activeClass : inActiveClass} href='/tops'>
-            <Dropdown.Item className='px-0 pt-0 pb-0'>Tops</Dropdown.Item>
+            <Dropdown.Item className='px-0 pt-0 pb-0 dark:hover:bg-[#5b21b6]'>Tops</Dropdown.Item>
         </Link>
         <Link className={router.asPath === '/about-us' ? activeClass : inActiveClass} href='/about-us'>
-            <Dropdown.Item className='px-0 pt-0 pb-0'>About Us</Dropdown.Item>
+            <Dropdown.Item className='px-0 pt-0 pb-0 dark:hover:bg-[#5b21b6]'>About Us</Dropdown.Item>
         </Link>
         <hr />
-        <Dropdown.Item className='px-0 pt-0 pb-0'>
+        <Dropdown.Item className='px-0 pt-0 pb-0 '>
             <button className='text-3xl w-full px-4 py-2 flex justify-center text-yellow-400 dark:text-violet-500' onClick={toggleTheme}>
                 {
                     theme === 'dark' ? <MdDarkMode /> : <MdLightMode />
--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,17 +1,21 @@
-import Link from 'next/link'
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useCallback, useContext, useEffect, useMemo, useState } from 'react'
 import { ThemeContext } from '../context/ThemeContext'
 import { MdLightMode, MdDarkMode } from 'react-icons/md'
 import { useRouter } from 'next/router';
-import { Dropdown } from "flowbite-react";
+import Link from 'next/link'
 import DropDownMenu from './DropDownMenu';
 
-export default function NavBar() {
+export default function NavBar() 
+{
     const { theme, toggleTheme } = useContext(ThemeContext);
     const router = useRouter();
+
     const [width, setWidth] = useState(null);
-    const activeClass = useMemo(() => 'font-bold text-xl dark:text-violet-800 transition-all duration-300', []);
     const widthListener = useCallback(() => setWidth(window.innerWidth), [],)
+
+    const activeClass = useMemo(() => 'font-bold text-xl dark:text-violet-800 transition-all duration-300', []);
+    const inActiveClass = useMemo(() => 'hover:font-bold hover:text-xl transition-all duration-300', []);
 
     useEffect(() => {
         setWidth(window.innerWidth)
@@ -19,11 +23,10 @@ export default function NavBar() {
 
     useEffect(() => {
         window.addEventListener('resize', widthListener)
-        console.log(width);
         return (() => {
             window.removeEventListener('resize', widthListener);
         })
-    }, [width])
+    }, [])
 
 
     return <nav className='dark:bg-black dark:text-white shadow-md px-8 py-4  fixed top-0 left-0 right-0 flex items-center justify-between'>
@@ -33,30 +36,28 @@ export default function NavBar() {
 
         {
             width <= 700 ? <DropDownMenu />
-                : <ul className='flex text-lg font-medium gap-x-4'>
-                    <li>
-                        <Link className={router.asPath === '/' ? activeClass : 'hover:font-bold hover:text-xl transition-all duration-300'} href='/'>Home</Link>
-                    </li>
-                    <li>
-                        <Link className={router.asPath === '/events' ? activeClass : 'hover:font-bold hover:text-xl transition-all duration-300'} href='/events'>Events</Link>
-                    </li>
-                    <li>
-                        <Link className={router.asPath === '/tops' ? activeClass : 'hover:font-bold hover:text-xl transition-all duration-300'} href='/tops'>Tops</Link>
-                    </li>
-                    <li>
-                        <Link className={router.asPath === '/about-us' ? activeClass : 'hover:font-bold hover:text-xl transition-all duration-300'} href='/about-us'>About Us</Link>
-                    </li>
-                    <li className='flex items-center pl-4'>
-                        <button className='text-3xl text-yellow-400 dark:text-violet-800' onClick={toggleTheme}>
-                            {
-                                theme === 'dark' ? <MdDarkMode /> : <MdLightMode />
-                            }
-                        </button>
-                    </li>
-                </ul>
+            : <ul className='flex text-lg font-medium gap-x-4'>
+                <li>
+                    <Link className={router.asPath === '/' ? activeClass : inActiveClass} href='/'>Home</Link>
+                </li>
+                <li>
+                    <Link className={router.asPath === '/events' ? activeClass : inActiveClass} href='/events'>Events</Link>
+                </li>
+                <li>
+                    <Link className={router.asPath === '/tops' ? activeClass : inActiveClass} href='/tops'>Tops</Link>
+                </li>
+                <li>
+                    <Link className={router.asPath === '/about-us' ? activeClass : inActiveClass} href='/about-us'>About Us</Link>
+                </li>
+                <li className='flex items-center pl-4'>
+                    <button className='text-3xl text-yellow-400 dark:text-violet-800' onClick={toggleTheme}>
+                        {
+                            theme === 'dark' ? <MdDarkMode /> : <MdLightMode />
+                        }
+                    </button>
+                </li>
+            </ul>
         }
-
-
 
     </nav>
 
