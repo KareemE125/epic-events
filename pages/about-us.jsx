e969aba361daddaaa259eb11ae6f7932b035aedd
KareemE125 <kareemezzat.125@gmail.com>
2023-01-02 21:03:39 +0200

    text fix

--- pages/about-us.jsx
+++ pages/about-us.jsx
@@ -8,7 +8,7 @@ export default function Aboutus() {
     <h3 className='text-4xl font-bold text-center mb-8'> Welcome To Epic Events </h3>
     <p className='text-2xl mb-10'>
       Here you can discover The upcoming Epic Events and do not miss them out 😉. <br />
-      Epic Events is a Professional Events Platform. Here we will provide you only interesting content, which you will like very much. We're dedicated to providing you the best of Events, with a focus on dependability and show different events. We're working to turn our passion for Events into a booming online website. We hope you enjoy our Events as much as we enjoy offering them to you.
+      Epic Events is a Professional Events Platform. Here we will provide you only interesting content, which you will like very much. We are dedicated to providing you the best of Events, with a focus on dependability and show different events. We are working to turn our passion for Events into a booming online website. We hope you enjoy our Events as much as we enjoy offering them to you.
       <br /> <br />
       I will keep posting more important posts on my Website for all of you. Please give your support and love.
     </p>
